c4be9953ded0bbc0c2171a6a253aae218bd9ee95
kharlamovegor <kharlamovegormain@mail.ru>
2024-08-14 21:29:01 +0400

    base config for system

---
+++ nvim/lua/configs/conform.lua
@@ -0,0 +1,20 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettierd" },
+    html = { "prettierd" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    javascript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    rust = { "rustfmt" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require("conform").setup(options)
---
+++ nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,37 @@
+-- EXAMPLE
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+  "html",
+  "cssls",
+  "emmet_language_server",
+  "eslint",
+  "pyright",
+  "docker_compose_language_service",
+  "dockerls",
+  "jsonls",
+  "css_variables",
+  "cssmodules_ls",
+  "yamlls",
+  "graphql",
+  "rust_analyzer",
+}
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
